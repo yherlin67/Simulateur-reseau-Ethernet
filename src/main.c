0ee643293748dff7f8453d8ba35a2d6d06a691be
yschwartz <yannoushka57@gmail.com>
2026-06-02 15:07:02 +0200

    nom méthodes français -> anglais

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include "network.h"
 
-uint32_t convertirIP(const char *ipStr) {
+uint32_t convertIP(const char *ipStr) {
     unsigned int octet1, octet2, octet3, octet4;
     
     if (sscanf(ipStr, "%u.%u.%u.%u", &octet1, &octet2, &octet3, &octet4) != 4) {
@@ -19,7 +19,7 @@ uint32_t convertirIP(const char *ipStr) {
     return ip_bin;
 }
 
-uint64_t convertirMAC(const char *macStr) {
+uint64_t convertMAC(const char *macStr) {
     unsigned int m1, m2, m3, m4, m5, m6;
     
     if (sscanf(macStr, "%x:%x:%x:%x:%x:%x", &m1, &m2, &m3, &m4, &m5, &m6) != 6) {
@@ -36,14 +36,14 @@ uint64_t convertirMAC(const char *macStr) {
     return mac_bin;
 }
 
-void afficherIPBinaire(uint32_t ip) {
+void displayBinaryIP(uint32_t ip) {
     for (int i = 0; i < 32; i++) {
         uint32_t bit = (ip >> (31 - i)) & 1;
         printf("%u", bit);
     }
 }
 
-void afficherMACBinaire(uint64_t mac) {
+void displayBinaryMAC(uint64_t mac) {
     for (int i = 0; i < 48; i++) {
         uint64_t bit = (mac >> (47 - i)) & 1;
         printf("%u", (unsigned int)bit);
@@ -51,32 +51,32 @@ void afficherMACBinaire(uint64_t mac) {
 }
 
 
-void lireFichierConfiguration(const char *cheminFichier, struct reseau *res) {
-    FILE *fichier = fopen(cheminFichier, "r");
+void ReadFile(const char *pathFile, struct reseau *res) {
+    FILE *file = fopen(pathFile, "r");
     
-    if (fichier == NULL) {
-        printf("Erreur : Impossible d'ouvrir le fichier '%s'.\n", cheminFichier);
+    if (file == NULL) {
+        printf("Erreur : Impossible d'ouvrir le fichier '%s'.\n", pathFile);
         return;
     }
-    char ligne[256];
+    char line[256];
 
-    int nbEquipements = 0;
+    int equipments = 0;
     int nbLiens = 0;
 
-    if (fgets(ligne, sizeof(ligne), fichier) != NULL) {
-        sscanf(ligne, "%d %d", &nbEquipements, &nbLiens);
+    if (fgets(line, sizeof(line), file) != NULL) {
+        sscanf(line, "%d %d", &equipments, &nbLiens);
     }
     printf("================ EN-TÊTE ================\n");
-    printf("Nombre d'equipements attendus : %d\n", nbEquipements);
+    printf("Nombre d'equipements attendus : %d\n", equipments);
     printf("Nombre de liens attendus       : %d\n\n", nbLiens);
     res->nbLiens = nbLiens;
     res->liens = malloc(nbLiens * sizeof(struct lien*));
 
     printf("============== ÉQUIPEMENTS ==============\n");
-    for (int i = 0; i < nbEquipements; i++) {
-        if (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+    for (int i = 0; i < equipments; i++) {
+        if (fgets(line, sizeof(line), file) != NULL) {
             int typeEquipement = 0;
-            sscanf(ligne, "%d", &typeEquipement);
+            sscanf(line, "%d", &typeEquipement);
             
             char macStr[32] = {0};
             char ipStr[32] = {0};
@@ -86,7 +86,7 @@ void lireFichierConfiguration(const char *cheminFichier, struct reseau *res) {
                 int nbPorts = 0;
                 int priorite = 0;
 
-                sscanf(ligne, "%*d;%[^;];%d;%d", macStr, &nbPorts, &priorite);
+                sscanf(line, "%*d;%[^;];%d;%d", macStr, &nbPorts, &priorite);
                 adresseMac = convertirMAC(macStr);
 
                 printf("[ID %d] SWITCH  -> MAC Brut: %s\n", i, macStr);
@@ -98,7 +98,7 @@ void lireFichierConfiguration(const char *cheminFichier, struct reseau *res) {
                 uint64_t adresseMac = 0;
                 uint32_t adresseIp = 0;
 
-                sscanf(ligne, "%*d;%[^;];%[^;\n]", macStr, ipStr);
+                sscanf(line, "%*d;%[^;];%[^;\n]", macStr, ipStr);
                 adresseMac = convertirMAC(macStr);
                 adresseIp = convertirIP(ipStr);
 
@@ -114,12 +114,12 @@ void lireFichierConfiguration(const char *cheminFichier, struct reseau *res) {
 
     printf("================= LIENS =================\n");
     for (int i = 0; i < nbLiens; i++) {
-        if (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        if (fgets(line, sizeof(line), file) != NULL) {
             int idEquipement1 = 0;
             int idEquipement2 = 0;
             int coutLien = 0;
 
-            sscanf(ligne, "%d;%d;%d", &idEquipement1, &idEquipement2, &coutLien);
+            sscanf(line, "%d;%d;%d", &idEquipement1, &idEquipement2, &coutLien);
             printf("Lien n°%d : Equipement %d <---> Equipement %d (Cout: %d)\n", i + 1, idEquipement1, idEquipement2, coutLien);
             res->liens[i] = malloc(sizeof(struct lien));
             if (res->liens[i] == NULL) {
@@ -142,7 +142,12 @@ void lireFichierConfiguration(const char *cheminFichier, struct reseau *res) {
         }
     }
 
-    fclose(fichier);
+    fclose(file);
+}
+
+void sendFrame(char message)
+{
+
 }
 
 int main() {
@@ -160,6 +165,6 @@ int main() {
         free(res.liens[i]);
     }
     free(res.liens);
-    
+
     return 0;
 }
